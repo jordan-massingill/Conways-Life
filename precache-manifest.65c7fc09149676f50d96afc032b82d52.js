e9f78b3e33dab96b7a6f6bbf2dba26d40b3e6b77
jordan-massingill <jordan_massingill@hotmail.com>
2018-12-13 17:21:10 -0600

    Updates

---
+++ precache-manifest.65c7fc09149676f50d96afc032b82d52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d533ab09956ceb10fb",
+    "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
+  },
+  {
+    "revision": "431fc896e1c6c678d0d5",
+    "url": "/Conways-Life/static/js/main.431fc896.chunk.js"
+  },
+  {
+    "revision": "404b80ed86825195d001",
+    "url": "/Conways-Life/static/js/1.404b80ed.chunk.js"
+  },
+  {
+    "revision": "431fc896e1c6c678d0d5",
+    "url": "/Conways-Life/static/css/main.8a9a65ea.chunk.css"
+  },
+  {
+    "revision": "a0177fc16152ec970501b81bd1047373",
+    "url": "/Conways-Life/index.html"
+  }
+];
